25212415332817e6ee104929b857c89822699996
caltuntas <cihat.altuntas@gmail.com>
2024-05-30 09:09:10 +0300

    c:resistor_color_duo

--- c/resistor-color-duo/resistor_color_duo.c
+++ c/resistor-color-duo/resistor_color_duo.c
@@ -2,16 +2,5 @@
 #include "resistor_color_duo.h"
 
 uint16_t color_code(resistor_band_t r[]){
-  char str[10];
-  str[0] = '\0';
-  resistor_band_t *p = r;
-  int i = 0;
-  while(*p >= BLACK && *p <= WHITE && i < 2) {
-    char c = *p + '0';
-    printf("%c=",c);
-    strncat(str,&c,1);
-    p++;
-    i++;
-  }
-  return strtol(str,NULL, 10);
+  return r[0]*10 + r[1];
 }
