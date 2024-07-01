48b8529024d16fa1bfbf9ffdf21a29378a561359
caltuntas <cihat.altuntas@gmail.com>
2024-07-01 14:18:12 +0300

    c:prime_factors

--- c/prime-factors/prime_factors.c
+++ c/prime-factors/prime_factors.c
@@ -3,7 +3,6 @@
 size_t find_factors(uint64_t n, uint64_t factors[static MAXFACTORS])
 {
   uint64_t left = n;
-  //for (uint64_t i=2; i<n; i++)
   int divisor = 2;
   size_t count =0;
   while (left > 1 )
