6daf48b4705615d01a2fe79a573a45299d4a3d7d
caltuntas <cihat.altuntas@gmail.com>
2024-07-01 11:21:49 +0300

    c:beer_song

--- c/beer-song/beer_song.c
+++ c/beer-song/beer_song.c
@@ -11,27 +11,19 @@ void recite(uint8_t start_bottles, uint8_t take_down, char **song)
     char* take_down_bottle = "Take one down and pass it around, %d bottle of beer on the wall.";
     char* take_down_no_more = "Take it down and pass it around, no more bottles of beer on the wall.";
     char* take_down_store = "Go to the store and buy some more, 99 bottles of beer on the wall.";
-    char *line1 = calloc(100,sizeof *line1);
-    char *line2 = calloc(100,sizeof *line1);
-    char *empty = calloc(1,sizeof(char));
     if(i>1)
-      sprintf(line1,bottles_of,i,i);
+      sprintf(song[j],bottles_of,i,i);
     if(i == 1)
-      sprintf(line1,bottle_of,i,i);
+      sprintf(song[j],bottle_of,i,i);
     if(i == 0)
-      sprintf(line1,"%s",bottle_of_no_more);
+      sprintf(song[j],"%s",bottle_of_no_more);
     if(i-1>1)
-      sprintf(line2,take_down_bottles,i-1);
+      sprintf(song[j+1],take_down_bottles,i-1);
     if(i-1 == 1)
-      sprintf(line2,take_down_bottle,i-1);
+      sprintf(song[j+1],take_down_bottle,i-1);
     if(i-1 == 0)
-      sprintf(line2,"%s",take_down_no_more);
+      sprintf(song[j+1],"%s",take_down_no_more);
     if(i-1 < 0)
-      sprintf(line2,"%s",take_down_store);
-
-
-    song[j] = line1;
-    song[j+1] = line2;
-    song[j+2] = empty;
+      sprintf(song[j+1],"%s",take_down_store);
   }
 }
