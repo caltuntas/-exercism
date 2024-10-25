f015df006c4a117dcaf4efd6db4ae53da029823a
caltuntas <cihat.altuntas@gmail.com>
2024-10-25 13:53:09 +0300

    c:atbash_cipher

--- c/atbash-cipher/atbash_cipher.c
+++ c/atbash-cipher/atbash_cipher.c
@@ -8,17 +8,15 @@
 char *atbash_encode(const char *input) {
   size_t len = strlen(input);
   char* encoded = calloc(len*2,sizeof(char));
-
   size_t counter =1,spaces =0;
   bool add_space = false;
+
   for(size_t i=0; i<len; i++) {
     char c = tolower(input[i]);
-    if(!isalpha(c) && !isdigit(c)) 
-      continue;
+    if(!isalpha(c) && !isdigit(c)) continue;
 
     if (isalpha(c)) {
-      int diff = 'z'-c;
-      c = 'a' + diff;
+      c = 'a' + 'z' - c;
     }
 
     if (add_space) {
@@ -32,24 +30,20 @@ char *atbash_encode(const char *input) {
     add_space = (counter-spaces) % 5 == 0;
     counter++;
   }
-
   return encoded;
 }
+
 char *atbash_decode(const char *input) {
   size_t len = strlen(input);
-  char* decoded = calloc(len*2,sizeof(char));
+  char* decoded = calloc(len,sizeof(char));
   size_t counter =0;
   for(size_t i=0; i<len; i++) {
     char c = tolower(input[i]);
     if(isalpha(c) || isdigit(c)) {
-      char chr;
-      if(isdigit(c)) {
-        chr = c;
-      }else {
-        int diff = 'z'-c;
-        chr = 'a' + diff;
+      if(isalpha(c)) {
+        c = 'a' + 'z' - c;
       }
-      decoded[counter] = chr;
+      decoded[counter] = c;
       counter++;
     }
   }
